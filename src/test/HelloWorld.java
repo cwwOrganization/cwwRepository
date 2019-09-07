3331c6e67d8731e92f7316096db2bbacc77f7750
cww <chenxxvv@foxmail.com>
2019-09-07 14:18:23 +0800

    测试分值修改

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,6 +2,7 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world 随便改一下");
+		System.out.println("hello world 随便改一下 ");
+		//这是测试分值的修改
 	}
 }
